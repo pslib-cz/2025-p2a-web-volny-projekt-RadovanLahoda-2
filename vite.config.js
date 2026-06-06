c483eeaf8c4ee5641b4832f8bb475c7136b3eddf
Radovan Lahoda <radovan.lahoda.024@pslib.cz>
2026-06-06 11:38:55 +0200

    oprava gh-pages (snad)

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import {defineConfig} from "vite";
 
-export default defineConfig({base: "https://github.com/pslib-cz/2025-p2a-web-volny-projekt-RadovanLahoda-2.git"});
+export default defineConfig({base: "/https://github.com/pslib-cz/2025-p2a-web-volny-projekt-RadovanLahoda-2.git/"});
