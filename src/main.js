1792af7a149b397f094654f8f8f5a4344086f5b2
Radovan Lahoda <radovan.lahoda.024@pslib.cz>
2026-06-07 21:10:56 +0200

    I HATE VITE (removed viterror)

--- src/main.js
+++ src/main.js
@@ -1,28 +1,11 @@
-
-import Swiper from "swiper";
-import { Navigation, Pagination } from 'swiper/modules'
-
-import "normalize.css";
-import "../style/style.css";
-import "../style/swiper.css";
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-if (document.querySelector('.swiper')) {
-
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation],
   direction: "horizontal",
-  slidesPerView: 1.5,
+  slidesPerView: "auto",
   centeredSlides: true,
-  effect: "slide",
+  spaceBetween: 8,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   }
 });
-
-}
